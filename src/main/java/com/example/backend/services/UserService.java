848505c97265a088d0d03dd9814ffee354026f15
bilel zaaraoui <72788978+Stinger1369@users.noreply.github.com>
2025-02-17 15:43:25 +0100

    2025-02-17-15h43-bi

---
+++ src/main/java/com/example/backend/services/UserService.java
@@ -0,0 +1,32 @@
+package com.example.backend.services;
+
+import com.example.backend.models.User;
+import com.example.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
